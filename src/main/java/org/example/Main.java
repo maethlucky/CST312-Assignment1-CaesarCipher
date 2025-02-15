4561dc1a491a7f81b060e143ebefaa6def7bb161
maethlucky <123992696+maethlucky@users.noreply.github.com>
2025-02-14 18:08:03 -0800

    Implemented writing to an output file.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ a brute force attack and a dictionary.
  */
 
 // === IMPORTS ===
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileReader;
@@ -58,6 +59,9 @@ public class Main {
         Scanner in = new Scanner (System.in);
         System.out.print("Please enter the name of the file you want to decode: ");
         String filename = in.nextLine().trim();
+        // This is the file the program will write to
+        // (Note: I used ChatGPT to get this regex to change the filename)
+        String outputFilename = filename.replaceFirst("(\\.\\w+)?$", "-solution.txt");
 
         try {
             FileReader fr = new FileReader(filename);
@@ -82,54 +86,70 @@ public class Main {
             splitTexts.add(text.toCharArray());
         }
 
-        // Brute forcing all possible ciphers and testing against the dictionary
-        for (char[] text : splitTexts) {
-            // Testing once per possible letter
-            for (int i = 1; i <= 26; i++) {
-                // Shifting letters
-                for (int j = 0; j < text.length; j++) {
-                    // If c = 'z' go to 'a', else shift one to the right
-                    if (text[j] != ' ') {
-                        if (text[j] == 'z') {
-                            text[j] = 'a';
-                        } else {
-                            text[j] += 1;
+        // Preparing to write to file
+        try {
+            FileWriter fw = new FileWriter(outputFilename);
+
+            // Brute forcing all possible ciphers and testing against the dictionary
+            for (char[] text : splitTexts) {
+                // Testing once per possible letter
+                for (int i = 1; i <= 26; i++) {
+                    // Shifting letters
+                    for (int j = 0; j < text.length; j++) {
+                        // If c = 'z' go to 'a', else shift one to the right
+                        if (text[j] != ' ') {
+                            if (text[j] == 'z') {
+                                text[j] = 'a';
+                            } else {
+                                text[j] += 1;
+                            }
                         }
                     }
-                }
-                // Testing to see if words are in the dictionary
-                shiftText = new String(text);
-                count = 0;
-                flag = false;
-                for (String word : shiftText.split(" ")) {
-                    if (dictionary.contains(word)) {
-                        count++;
+                    // Testing to see if words are in the dictionary
+                    shiftText = new String(text);
+                    count = 0;
+                    flag = false;
+                    for (String word : shiftText.split(" ")) {
+                        if (dictionary.contains(word)) {
+                            count++;
+                        }
+                    }
+                    // If over 75% of the words in the shifted text are in the
+                    // dictionary, saves the shift and breaks.
+                    if (count > ((shiftText.split(" ").length) / 4) * 3) {
+                        if (i == 26) {
+                            shift = 0;
+                        } else {
+                            shift = i;
+                        }
+                        flag = true;
+                        break;
                     }
                 }
-                // If over 75% of the words in the shifted text are in the
-                // dictionary, saves the shift and breaks.
-                if (count > ((shiftText.split(" ").length) / 4) * 3) {
-                    if (i == 26) {
-                        shift = 0;
-                    } else {
-                        shift = i;
-                    }
-                    flag = true;
-                    break;
+
+                // === FILE WRITING ===
+
+                // If a shifted text where over 75% of the words were in
+                // the dictionary was found, shows that text and the shift amount,
+                // then writes the results to a new file.
+                // If not, displays an error message.
+                if (flag) {
+                    System.out.println(shift + ";" + shiftText);
+                    fw.write(shift + ";" + shiftText + "\n");
+                } else {
+                    System.out.println("No decoded text could be found.");
+                    fw.write("No decoded text could be found.\n");
                 }
+
+
+
             }
 
-            // === FILE WRITING ===
+            fw.close();
 
-            // If a shifted text where over 75% of the words were in
-            // the dictionary was found, shows that text and the shift amount.
-            // If not, displays an error message.
-            if (flag) {
-                System.out.println(shift + ";" + shiftText);
-            } else {
-                System.out.println("No decoded text could be found.");
-            }
+        } catch (Exception e) { // Catches errors with writing to the file
+            System.out.println(outputFilename + " could not be written to, closing program");
+            System.exit(1);
         }
-
     }
 }
